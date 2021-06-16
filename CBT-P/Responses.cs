4e38d0420b34123702099febdbef431f49aa836b
Garrett Stonis <GStonis@emduggan.com>
2021-06-16 16:16:50 -0400

    Added the Respnonses enum.

---
+++ CBT-P/Responses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CBT_P
+{
+    /// <summary>
+    /// The available responses for CBT-P.
+    /// </summary>
+    public enum Responses
+    {
+        /// <summary>
+        /// When a movement command was received and commenced.
+        /// </summary>
+        Ok,
+
+        /// <summary>
+        /// When an unkown command was received.
+        /// </summary>
+        Bad
+    }
+}
